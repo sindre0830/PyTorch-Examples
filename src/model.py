47447a1b16d11aebc4c0378219866be925607e43
sindre0830 <sindre0830@gmail.com>
2022-06-21 23:42:21 +0200

    Add layers to Model class #3

--- src/model.py
+++ src/model.py
@@ -3,6 +3,50 @@ import torch
 
 
 # Defines the machine learning model layout.
+# source: https://towardsdatascience.com/going-beyond-99-mnist-handwritten-digits-recognition-cfff96337392
 class Model(torch.nn.Module):
     def __init__(self):
         super().__init__()
+        self.layer1 = torch.nn.Sequential(
+            torch.nn.Conv2d(in_channels=1, out_channels=32, kernel_size=5, stride=1),
+            torch.nn.ReLU(inplace=True)
+        )
+        self.layer2 = torch.nn.LazyConv2d(out_channels=32, kernel_size=5, stride=1, bias=False)
+        self.layer3 = torch.nn.Sequential(
+            torch.nn.LazyBatchNorm2d(),
+            torch.nn.ReLU(inplace=True),
+            torch.nn.MaxPool2d(kernel_size=2, stride=2),
+            torch.nn.Dropout(.25, inplace=True)
+        )
+        self.layer4 = torch.nn.Sequential(
+            torch.nn.LazyConv2d(out_channels=64, kernel_size=3, stride=1),
+            torch.nn.ReLU(inplace=True),
+        )
+        self.layer5 = torch.nn.LazyConv2d(out_channels=64, kernel_size=3, stride=1, bias=False)
+        self.layer6 = torch.nn.Sequential(
+            torch.nn.LazyBatchNorm2d(),
+            torch.nn.ReLU(inplace=True),
+            torch.nn.MaxPool2d(kernel_size=2, stride=2),
+            torch.nn.Dropout(.25, inplace=True)
+        )
+        self.flatten = torch.nn.Flatten()
+        self.layer7 = torch.nn.LazyLinear(out_features=256, bias=False)
+        self.layer8 = torch.nn.Sequential(
+            torch.nn.LazyBatchNorm1d(),
+            torch.nn.ReLU(inplace=True)
+        )
+        self.layer9 = torch.nn.LazyLinear(out_features=128, bias=False)
+        self.layer10 = torch.nn.Sequential(
+            torch.nn.LazyBatchNorm1d(),
+            torch.nn.ReLU(inplace=True)
+        )
+        self.layer11 = torch.nn.LazyLinear(out_features=84, bias=False)
+        self.layer12 = torch.nn.Sequential(
+            torch.nn.LazyBatchNorm1d(),
+            torch.nn.ReLU(inplace=True),
+            torch.nn.Dropout(.25, inplace=True)
+        )
+        self.layer11 = torch.nn.Sequential(
+            torch.nn.LazyLinear(out_features=10),
+            torch.nn.Softmax()
+        )
