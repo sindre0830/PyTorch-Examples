469d500d7327a49fe4d38665df212fbfe35e7453
sindre0830 <sindre0830@gmail.com>
2022-06-22 13:08:17 +0200

    Update layer names #3

--- src/model.py
+++ src/model.py
@@ -7,62 +7,63 @@ import torch
 class Model(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.layer1 = torch.nn.Sequential(
+        self.input = torch.nn.Sequential(
             torch.nn.Conv2d(in_channels=1, out_channels=32, kernel_size=5, stride=1),
             torch.nn.ReLU(inplace=True)
         )
-        self.layer2 = torch.nn.LazyConv2d(out_channels=32, kernel_size=5, stride=1, bias=False)
-        self.layer3 = torch.nn.Sequential(
+        self.conv2d_1 = torch.nn.LazyConv2d(out_channels=32, kernel_size=5, stride=1, bias=False)
+        self.conv2d_2 = torch.nn.Sequential(
             torch.nn.LazyBatchNorm2d(),
             torch.nn.ReLU(inplace=True),
             torch.nn.MaxPool2d(kernel_size=2, stride=2),
             torch.nn.Dropout(.25, inplace=True)
         )
-        self.layer4 = torch.nn.Sequential(
+        self.conv2d_3 = torch.nn.Sequential(
             torch.nn.LazyConv2d(out_channels=64, kernel_size=3, stride=1),
             torch.nn.ReLU(inplace=True),
         )
-        self.layer5 = torch.nn.LazyConv2d(out_channels=64, kernel_size=3, stride=1, bias=False)
-        self.layer6 = torch.nn.Sequential(
+        self.conv2d_4 = torch.nn.LazyConv2d(out_channels=64, kernel_size=3, stride=1, bias=False)
+        self.conv2d_5 = torch.nn.Sequential(
             torch.nn.LazyBatchNorm2d(),
             torch.nn.ReLU(inplace=True),
             torch.nn.MaxPool2d(kernel_size=2, stride=2),
             torch.nn.Dropout(.25, inplace=True)
         )
         self.flatten = torch.nn.Flatten()
-        self.layer7 = torch.nn.LazyLinear(out_features=256, bias=False)
-        self.layer8 = torch.nn.Sequential(
+        self.linear_1 = torch.nn.LazyLinear(out_features=256, bias=False)
+        self.linear_2 = torch.nn.Sequential(
             torch.nn.LazyBatchNorm1d(),
             torch.nn.ReLU(inplace=True)
         )
-        self.layer9 = torch.nn.LazyLinear(out_features=128, bias=False)
-        self.layer10 = torch.nn.Sequential(
+        self.linear_3 = torch.nn.LazyLinear(out_features=128, bias=False)
+        self.linear_4 = torch.nn.Sequential(
             torch.nn.LazyBatchNorm1d(),
             torch.nn.ReLU(inplace=True)
         )
-        self.layer11 = torch.nn.LazyLinear(out_features=84, bias=False)
-        self.layer12 = torch.nn.Sequential(
+        self.linear_5 = torch.nn.LazyLinear(out_features=84, bias=False)
+        self.linear_6 = torch.nn.Sequential(
             torch.nn.LazyBatchNorm1d(),
             torch.nn.ReLU(inplace=True),
             torch.nn.Dropout(.25, inplace=True)
         )
-        self.layer13 = torch.nn.Sequential(
+        self.output = torch.nn.Sequential(
             torch.nn.LazyLinear(out_features=10),
             torch.nn.Softmax()
         )
 
     def forward(self, x):
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer5(x)
-        x = self.layer6(x)
+        x = self.input(x)
+        x = self.conv2d_1(x)
+        x = self.conv2d_2(x)
+        x = self.conv2d_3(x)
+        x = self.conv2d_4(x)
+        x = self.conv2d_5(x)
         x = self.flatten(x)
-        x = self.layer7(x)
-        x = self.layer8(x)
-        x = self.layer9(x)
-        x = self.layer10(x)
-        x = self.layer11(x)
-        x = self.layer12(x)
-        x = self.layer13(x)
+        x = self.linear_1(x)
+        x = self.linear_2(x)
+        x = self.linear_3(x)
+        x = self.linear_4(x)
+        x = self.linear_5(x)
+        x = self.linear_6(x)
+        x = self.output(x)
         return x
