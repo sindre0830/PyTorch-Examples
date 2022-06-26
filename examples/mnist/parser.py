8957b220a1e50bcb3763ccfed876cc82fb3a9345
sindre0830 <sindre0830@gmail.com>
2022-06-26 12:17:30 +0200

    Add header to init function in Dataset class #12

--- examples/mnist/parser.py
+++ examples/mnist/parser.py
@@ -2,5 +2,9 @@ class Dataset():
     """
     Class for parsing datasets for PyTorch machine learning purposes.
     """
+    
     def __init__(self):
+        """
+        Initializes dataset class.
+        """
         pass
